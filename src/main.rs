e28cc6a9cb8e20e7fb5441f84daf353a5bd7868f
Jose Gutierrez <gutiory@gmail.com>
2025-07-09 09:54:12 +0200

    Add generate_password method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rand::{distr::Alphanumeric, prelude::*};
+
 fn main() {
-    println!("Hello, world!");
+    let pass = generate_password(13);
+    println!("Hola holita, contraseña: {}", pass);
+}
+
+fn generate_password(size: usize) -> String {
+    let rng = rand::rng();
+    rng.sample_iter(Alphanumeric)
+        .take(size)
+        .inspect(|value| println!("{}, ", value))
+        .map(char::from)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::generate_password;
+
+    #[test]
+    fn test_generate_password() {
+        let size: usize = 15;
+        assert_eq!(generate_password(size).chars().count(), size)
+    }
 }
